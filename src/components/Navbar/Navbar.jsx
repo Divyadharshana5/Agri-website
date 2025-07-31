bef39b32b29d53a69a37ebedaf5ffea2e7b63002
janani k <145999085+Jananikaavya@users.noreply.github.com>
2025-07-31 13:56:38 +0530

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import './Navbar.css';
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src="/logo.png" alt="Logo" className="logo" />
+        <h1 className="site-title"></h1>
+      </div>
+    
+    </nav>
+  );
+};
+
+export default Navbar;
