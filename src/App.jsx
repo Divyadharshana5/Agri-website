4d61dc6181e7b305d5757b6c19cdcab8e714f839
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-31 14:42:19 +0530

    Refactor index.html and App.jsx; update title and add Language component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Home from'./components/Home/Home';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import "./App.css";
+import Language from "./components/Language/Language";
 
 function App() {
   return (
     <div className="app-container">
       <Navbar />
-      <Home/>
+      <Home />
+      <Language />
     </div>
   );
 }
