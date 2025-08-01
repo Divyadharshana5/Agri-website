449e790edec6cd5511fc1a6fece4293441284555
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-08-01 14:06:13 +0000

    updated

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,7 +5,31 @@ import logo from "../../../public/logo.png"; // Adjust path if needed
 const Register = () => {
   return (
     <div className="register-bg">
-      <div className="glass-card"></div>
+      <div className="glass-card">
+        <img src={logo} alt="Logo" className="login-logo" />
+                <h1 className="login-title">Login</h1>
+                <form className="login-form">
+                  <label htmlFor="username" className="login-label">
+                    Username:
+                  </label>
+                  <input type="text" id="username" className="login-input" />
+        
+                  <label htmlFor="password" className="login-label">
+                    Password:
+                  </label>
+                  <input type="password" id="password" className="login-input" />
+        
+                  <div className="login-links">
+                    <a href="#" className="forgot-link">
+                      Forgot password?
+                    </a>
+                  </div>
+        
+                  <button type="submit" className="login-submit">
+                    Submit
+                  </button>
+                </form>
+      </div>
       </div>
   )
 };
