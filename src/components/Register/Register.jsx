755003a18820b2f2f648a07ed806040a70946fbd
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-08-01 13:39:16 +0000

    updated

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Register.css";
+import logo from "../../../public/logo.png"; // Adjust path if needed
+
+const Register = () => {
+  return (
+    <div className="register-bg">
+      <div className="glass-card"></div>
+      </div>
+  )
+};
+export default Register;
