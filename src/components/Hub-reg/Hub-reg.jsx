82508c18dac65c3f19f8e8073be803429d7faf7d
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-08-01 15:17:50 +0000

    updated

---
+++ src/components/Hub-reg/Hub-reg.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Hub-reg.css";
+import logo from "../../../public/logo.png"; // Adjust path if needed
+
+const HubReg = () => {
+  return (
+    <div className="register-bg">
+      <div className="glass-card">
+        <img src={logo} alt="Logo" className="logo" />
+                <h1 className="title">Register</h1>
+                <form className="form">
+                  
+                  <label htmlFor="branch-id" className="label">
+                    Branch ID:
+                  </label>
+                  <input type="text" id="branch-id" className="input" />
+
+                   <label htmlFor="hub-name" className="label">
+                    Hub Name:
+                  </label>
+                  <input type="text" id="hub-name" className="input" />
+
+                   <label htmlFor="password" className="label">
+                    Password:
+                  </label>
+                  <input type="password" id="password" className="input" />
+
+                   <label htmlFor="confirm password" className="label">
+                    Confirm Password:
+                  </label>
+                  <input type="password" id="confirm password" className="input" />
+
+                    <button type="submit" className="button">Register</button>
+                </form>
+      </div>
+      </div>
+  )
+};
+export default HubReg;
